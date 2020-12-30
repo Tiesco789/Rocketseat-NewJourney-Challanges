ad68744784f9c7c6fa77a53f0b193aa741590230
Franccesco Antonio <38047906+Tiesco789@users.noreply.github.com>
2020-12-30 13:54:48 -0300

    master

---
+++ Rocketseat-NewJourney-Challange-2/src/app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+
+  return response.json(repositories);
+
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found'})
+  }
+
+  const likes = repositories[repoIndex].likes
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: likes
+  };
+
+  repositories[repoIndex] = repository;
+
+  return response.json(repository)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIndex >= 0) {
+    repositories.splice(repoIndex, 1);
+  } else {
+    return response.status(400).json({ error: 'Repository does not exists' })
+  }
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find((repository) => repository.id == id);
+  repository.likes.push(title);
+
+  if(!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1
+
+  return response.json(repositories)
+});
+
+module.exports = app;
---
+++ Rocketseat-NewJourney-Challange-6/src/services/CreateTransactionService.ts
@@ -0,0 +1,66 @@
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import Transaction from '../models/Transaction';
+
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    /**
+     * Verificar se a categoria já existe,
+     * se já existir, buscar ela do banco de dados que foi retornado,
+     * se não existir, criar uma nova
+     */
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
